908d372bc254053d17279d458a4a5d84a1ced128
sluzerpp <bistboy@list.ru>
2022-12-15 21:46:54 +0300

    feat: make cards draw function

--- online-store/src/components/views/render.ts
+++ online-store/src/components/views/render.ts
@@ -7,14 +7,36 @@ import { Product } from '../../domain/model';
 //после бизнес логики, и манипулировать домом для отрисовки
 export const renderProducts = (products: Product[]) => {
   console.log('renderingProducts');
-  const containerProducts = document.querySelector('.products-items');
-  if (!containerProducts) {
-    return;
-  }
-  console.log('rendering products');
-  containerProducts.innerHTML = '';
-  products.forEach((product) => {
-    containerProducts.innerHTML += product.brand + ' ';
-  });
+  draw(products);
   //какие то дом манипуляции
 };
+
+function draw(products: Product[]) {
+  const template: HTMLTemplateElement | null = document.querySelector('#card-template');
+  const fragment: DocumentFragment = document.createDocumentFragment();
+
+  products.forEach((item: Product) => {
+    const cardClone: HTMLTemplateElement | null = template?.content.cloneNode(true) as HTMLTemplateElement;
+    const cardImg: HTMLImageElement | null = cardClone.querySelector('.card__img > img');
+    if (cardImg !== null) {
+      cardImg.src = item.images[0];
+    }
+    const cardContent: HTMLElement | null = cardClone.querySelector('.card__content');
+    if (cardContent !== null) {
+      cardContent.innerHTML = 'Title: ' + item.title + '<br>';
+      cardContent.innerHTML += 'Category: ' + item.category + '<br>';
+      cardContent.innerHTML += 'Brand: ' + item.brand + '<br>';
+      cardContent.innerHTML += 'Price: ' + item.price + '<br>';
+      cardContent.innerHTML += 'Discount: ' + item.discountPercentage + '%<br>';
+      cardContent.innerHTML += 'Rating: ' + item.rating + '<br>';
+      cardContent.innerHTML += 'Stock: ' + item.stock + '<br>';
+    }
+
+    fragment.append(cardClone);
+  });
+  const containerProducts = document.querySelector('.products-items');
+  if (containerProducts) {
+    containerProducts.innerHTML = '';
+    containerProducts.appendChild(fragment);
+  }
+}
